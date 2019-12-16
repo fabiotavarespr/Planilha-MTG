325d597219b6b28d57f17b6d7eecdb4a0e71f904
Fabio Tavares Pereira Rego <fabiotavarespr@gmail.com>
2019-12-16 15:20:29 -0300

    Realizando testes com query

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/fabiotavarespr/Planilha-MTG/cmd"
+import (
+	"fmt"
 
-//"MagicTheGathering/mtg-sdk-go"
+	mtg "github.com/MagicTheGathering/mtg-sdk-go"
+)
+
+//import "github.com/fabiotavarespr/Planilha-MTG/cmd"
 
 func main() {
-	cmd.Execute()
+	qry := mtg.NewQuery()
+	fmt.Println(qry)
+	qry.Where(mtg.CardSet, "ELD").OrderBy(mtg.CardNumber)
+	fmt.Println(qry)
+	cards, err := qry.All()
+	fmt.Println(cards)
+	fmt.Println(err)
+
+	//	cmd.Execute()
 }
